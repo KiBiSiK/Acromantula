c8e48cd35269c21312e8190ca75276c494242908
Cydhra <ubezl@student.kit.edu>
2019-10-26 19:27:22 +0200

    implemented UNIX IPC

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/PipeServer.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/PipeServer.kt
@@ -2,8 +2,11 @@ package net.cydhra.acromantula.workspace
 
 import org.apache.commons.lang3.SystemUtils
 import org.apache.logging.log4j.LogManager
+import org.scalasbt.ipcsocket.UnixDomainServerSocket
 import org.scalasbt.ipcsocket.Win32NamedPipeServerSocket
 import java.net.ServerSocket
+import java.nio.file.Files
+
 
 /**
  * Serves a named pipe / unix socket for communication with potential front ends
@@ -26,10 +29,12 @@ class PipeServer {
                 server = Win32NamedPipeServerSocket(pipeName)
             }
             SystemUtils.IS_OS_UNIX -> {
-
+                val tempDir = Files.createTempDirectory("ipcsocket")
+                val sock = tempDir.resolve("api.sock")
+                server = UnixDomainServerSocket(sock.toString())
             }
             else -> {
-
+                throw IllegalStateException("this operating system is unsupported as no IPC can be provided")
             }
         }
     }
