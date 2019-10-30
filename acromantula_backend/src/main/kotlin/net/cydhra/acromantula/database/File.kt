8230480d3489fa8f8b9c871c9f0878f687e5bace
Cydhra <ubezl@student.kit.edu>
2019-10-31 00:47:59 +0100

    added column to file model

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/database/File.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/database/File.kt
@@ -8,4 +8,5 @@ object FileTable : IntIdTable("TreeFile") {
     val name = varchar("name", MAX_FILE_NAME)
     val parent = reference("parent", DirectoryTable).nullable()
     val archive = reference("archive", ArchiveTable).nullable()
+    val type = varchar("type", 31).nullable()
 }
