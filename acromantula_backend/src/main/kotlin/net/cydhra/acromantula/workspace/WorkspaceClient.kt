ab9adb9a90b667f266cb7c1ba5e6686715fea723
Cydhra <ubezl@student.kit.edu>
2019-12-28 18:37:50 +0100

    the workspace client now instantiates a worker pool

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceClient.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceClient.kt
@@ -1,6 +1,7 @@
 package net.cydhra.acromantula.workspace
 
 import net.cydhra.acromantula.workspace.commands.CommandDispatcher
+import net.cydhra.acromantula.workspace.worker.WorkerPool
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 abstract class WorkspaceClient {
@@ -11,5 +12,10 @@ abstract class WorkspaceClient {
     /**
      * The local command dispatcher that handles incoming requests for the workspace
      */
-    val commandDispatcher: CommandDispatcher = CommandDispatcher()
+    val commandDispatcher = CommandDispatcher()
+
+    /**
+     * The thread pools used for work related to workspace content
+     */
+    val workerPool = WorkerPool()
 }
