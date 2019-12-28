fd85132da243dbab225acc7bcdfc762ca80d0f6d
Cydhra <ubezl@student.kit.edu>
2019-12-28 18:51:28 +0100

    command dispatcher holds a reference to the workspace client which is invisible outside of the workspace service. The command dispatcher is the interface for the outside world for interaction with the workspace

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceClient.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceClient.kt
@@ -1,18 +1,17 @@
 package net.cydhra.acromantula.workspace
 
-import net.cydhra.acromantula.workspace.commands.CommandDispatcher
 import net.cydhra.acromantula.workspace.worker.WorkerPool
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
+/**
+ * A client that connects to the current workspace. It abstracts the physical location of the workspace, allowing
+ * opaque handling of local and remote connections. It offers all resources associated with the workspace connection,
+ * including, but not limited to, database access and worker-thread-pool.
+ */
 abstract class WorkspaceClient {
 
     abstract var database: TransactionManager
         protected set
-
-    /**
-     * The local command dispatcher that handles incoming requests for the workspace
-     */
-    val commandDispatcher = CommandDispatcher()
 
     /**
      * The thread pools used for work related to workspace content
--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceService.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceService.kt
@@ -1,8 +1,9 @@
 package net.cydhra.acromantula.workspace
 
 import net.cydhra.acromantula.bus.EventBroker
-import net.cydhra.acromantula.bus.events.ApplicationStartupEvent
 import net.cydhra.acromantula.bus.Service
+import net.cydhra.acromantula.bus.events.ApplicationStartupEvent
+import net.cydhra.acromantula.workspace.commands.CommandDispatcher
 import net.cydhra.acromantula.workspace.ipc.IPCServer
 
 /**
@@ -13,6 +14,23 @@ object WorkspaceService : Service {
 
     override val name: String = "workspace-service"
 
+    /**
+     * The command dispatcher that handles incoming requests for the workspace. Every request for the workspace is
+     * dispatched here.
+     */
+    lateinit var commandDispatcher: CommandDispatcher
+
+    /**
+     * The client of the current workspace connection. This is only used internally by the service and hidden from
+     * the outside world. Services from outside invoke [net.cydhra.acromantula.workspace.commands.WorkspaceCommand]s
+     * at the [commandDispatcher] for any requests.
+     */
+    private lateinit var workspaceClient: WorkspaceClient
+
+    /**
+     * A server for communication with frontend applications. This is independent of and exceeds the current workspace
+     * connection.
+     */
     private val pipeServer = IPCServer()
 
     /**
@@ -20,6 +38,9 @@ object WorkspaceService : Service {
      */
     override suspend fun initialize() {
         EventBroker.registerEventListener(ApplicationStartupEvent::class, this::onStartup)
+
+        workspaceClient = LocalWorkspaceClient()
+        commandDispatcher = CommandDispatcher(workspaceClient)
     }
 
     @Suppress("RedundantSuspendModifier", "UNUSED_PARAMETER")
