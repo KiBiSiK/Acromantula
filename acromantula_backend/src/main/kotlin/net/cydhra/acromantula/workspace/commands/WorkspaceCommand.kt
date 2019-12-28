e628f16e98434c0fc9cdc45120c1dcc26f9553d9
Cydhra <ubezl@student.kit.edu>
2019-12-28 19:14:59 +0100

    commands are events, too

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/commands/WorkspaceCommand.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/commands/WorkspaceCommand.kt
@@ -1,10 +1,9 @@
 package net.cydhra.acromantula.workspace.commands
 
-import net.cydhra.acromantula.bus.IPCSerializable
+import net.cydhra.acromantula.bus.Event
 
 /**
- * A command for the [CommandDispatcher] that invokes some kind of service within the Workspace.
+ * A command for the [CommandDispatcher] that invokes some kind of service within the Workspace. A command is a
+ * special kind of [Event].
  */
-interface WorkspaceCommand : IPCSerializable {
-
-}
+interface WorkspaceCommand : Event
