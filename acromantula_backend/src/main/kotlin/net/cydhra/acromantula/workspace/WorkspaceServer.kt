6e5e5904405c18aa38fbb1acc8d319ad43d93e25
Cydhra <ubezl@student.kit.edu>
2019-12-29 23:45:16 +0100

    WorkspaceServer stores the WorkspaceFileSystem

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceServer.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/workspace/WorkspaceServer.kt
@@ -1,5 +1,6 @@
 package net.cydhra.acromantula.workspace
 
+import net.cydhra.acromantula.workspace.files.WorkspaceFileSystem
 import org.h2.jdbcx.JdbcDataSource
 import java.io.File
 import javax.sql.DataSource
@@ -7,6 +8,11 @@ import javax.sql.DataSource
 class WorkspaceServer(private val workspaceFile: File) {
 
     private lateinit var dataSource: DataSource
+
+    /**
+     * The access to binary resources that are stored directly in the file system
+     */
+    private val workspaceFileSystem = WorkspaceFileSystem(workspaceFile)
 
     fun initialize() {
         dataSource = JdbcDataSource()
