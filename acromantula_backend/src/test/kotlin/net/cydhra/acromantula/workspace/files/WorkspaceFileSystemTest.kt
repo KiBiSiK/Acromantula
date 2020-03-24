34b90758bc2345bcd55ae95821d850ac99d252eb
Cydhra <ubezl@student.kit.edu>
2020-03-24 02:19:12 +0100

    setup and teardown for WFS unit tests

---
+++ acromantula_backend/src/test/kotlin/net/cydhra/acromantula/workspace/files/WorkspaceFileSystemTest.kt
@@ -0,0 +1,57 @@
+package net.cydhra.acromantula.workspace.files
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class WorkspaceFileSystemTest {
+
+    /**
+     * A temporary directory where to do the tests
+     */
+    private val wfsPath = File("wfstest")
+
+    /**
+     * The wfs instance to use in tests
+     */
+    private lateinit var wfs: WorkspaceFileSystem
+
+    @BeforeEach
+    fun setUp() {
+        if (wfsPath.exists())
+            wfsPath.delete()
+
+        wfsPath.apply(File::mkdir)
+        wfs = WorkspaceFileSystem(wfsPath)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        wfsPath.delete()
+    }
+
+    @Test
+    fun importResource() {
+    }
+
+    @Test
+    fun addResource() {
+    }
+
+    @Test
+    fun readResource() {
+    }
+
+    @Test
+    fun updateResource() {
+    }
+
+    @Test
+    fun deleteResource() {
+    }
+
+    @Test
+    fun exportResource() {
+    }
+}
