b09930acc668914fa4caf33185333e8d7d96c17e
Cydhra <ubezl@student.kit.edu>
2020-04-21 02:18:22 +0200

    added asm dependency

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/features/disassemble/Disassembler.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/features/disassemble/Disassembler.kt
@@ -1,9 +1,16 @@
 package net.cydhra.acromantula.features.disassemble
 
+import org.objectweb.asm.tree.ClassNode
+
 interface Disassembler {
     /**
      * A unique identifier for the disassembler that is later used to reference it in GUIs and commands. It must not
      * contain spaces.
      */
     val identifier: String
+
+    /**
+     * Disassemble a class node into text and meta information
+     */
+    fun disassemble(node: ClassNode)
 }
