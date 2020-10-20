5254d8d0699bc438d8d6aeb7b8ade26695c21eb2
Cydhra <ubezl@student.kit.edu>
2020-10-21 01:49:08 +0200

    command dispatch function implementation

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/commands/CommandDispatcher.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/commands/CommandDispatcher.kt
@@ -1,5 +1,8 @@
 package net.cydhra.acromantula.commands
 
+import kotlinx.coroutines.Job
+import net.cydhra.acromantula.workspace.WorkspaceService
+
 /**
  *
  */
@@ -9,7 +12,7 @@ object CommandDispatcher {
      * Dispatch a command that originates from anywhere at the workspace. Dispatching it will schedule the command to
      * the worker pool and generate a status code, that can be used to request status information about the command.
      */
-    fun dispatchCommand(command: WorkspaceCommand) {
-        // TODO
+    fun dispatchCommand(command: WorkspaceCommand): Job {
+        return WorkspaceService.getWorkerPool().launchTask { command.evaluate() }
     }
 }
