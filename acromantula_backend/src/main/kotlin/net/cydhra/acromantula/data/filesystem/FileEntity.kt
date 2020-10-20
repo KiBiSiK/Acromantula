9c66bce29920fb03f7b81d9eb7f2728b351552d1
Cydhra <ubezl@student.kit.edu>
2020-10-20 13:15:40 +0200

    files do not reference their archive, as an artificial directory entry is created for it

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/data/filesystem/FileEntity.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/data/filesystem/FileEntity.kt
@@ -8,7 +8,6 @@ import org.jetbrains.exposed.dao.IntIdTable
 object FileTable : IntIdTable("TreeFile") {
     val name = varchar("name", MAX_FILE_NAME)
     val parent = reference("parent", DirectoryTable).nullable()
-    val archive = reference("archive", ArchiveTable).nullable()
     val type = varchar("type", 31).nullable()
     val resource = integer("resource").nullable()
 }
@@ -21,7 +20,6 @@ class FileEntity(id: EntityID<Int>) : IntEntity(id) {
 
     var name by FileTable.name
     var parent by DirectoryEntity optionalReferencedOn FileTable.parent
-    var archive by ArchiveEntity optionalReferencedOn FileTable.archive
     var type by FileTable.type
     var resource by FileTable.resource
 }
