b2a26f260b2813fe14003a71ed440e6f772478d1
Cydhra <ubezl@student.kit.edu>
2020-10-20 02:25:22 +0200

    implementation to import archives

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/features/import/ArchiveImporterStrategy.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/features/import/ArchiveImporterStrategy.kt
@@ -1,9 +1,9 @@
 package net.cydhra.acromantula.features.import
 
-import net.cydhra.acromantula.data.DirectoryEntity
-import net.cydhra.acromantula.features.import.util.ArchiveTreeBuilder
+import net.cydhra.acromantula.data.filesystem.DirectoryEntity
+import net.cydhra.acromantula.workspace.WorkspaceService
 import org.apache.logging.log4j.LogManager
-import java.io.ByteArrayInputStream
+import java.io.InputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 
@@ -11,38 +11,75 @@ internal class ArchiveImporterStrategy : ImporterStrategy {
 
     companion object {
         private val logger = LogManager.getLogger()
+
+        private const val LAST_DIRECTORY_PATTERN = "[^\\/]*\\/\$"
+        private val lastDirectoryRegex = LAST_DIRECTORY_PATTERN.toRegex()
     }
 
-    override fun handles(fileName: String, fileContent: ByteArray): Boolean {
-        return try {
-            val zipIn = ZipInputStream(ByteArrayInputStream(fileContent))
-            true
-        } catch (e: Exception) {
-            false
-        }
+    private val directoryMap = mutableMapOf<String, DirectoryEntity>()
+
+    override fun handles(fileName: String, fileContent: InputStream): Boolean {
+        // TODO maybe search magic bytes idk
+        return fileName.endsWith(".zip") || fileName.endsWith(".jar")
     }
 
-    override fun import(parent: DirectoryEntity, fileName: String, fileContent: ByteArray) {
-        val zipInputStream = ZipInputStream(ByteArrayInputStream(fileContent))
-        val treeBuilder = ArchiveTreeBuilder(fileName)
+    override fun import(parent: DirectoryEntity, fileName: String, fileContent: InputStream) {
+        val archive = WorkspaceService.addArchiveEntry(fileName, parent)
+        directoryMap["/"] = archive
+
+        val zipInputStream = ZipInputStream(fileContent)
 
         var currentEntry: ZipEntry? = zipInputStream.nextEntry
         while (currentEntry != null) {
             logger.trace("importing ${currentEntry.name}")
 
-            // do not test for `sDirectory` explicitly here, as java accepts zip files whose folders contain file
+            // do not test for `isDirectory` explicitly here, as java accepts zip files whose folders contain file
             // content. Just check whether content is available and treat it as a file, if there is.
             if (zipInputStream.available() > 0) {
-                val entryContent: ByteArray = zipInputStream.readBytes()
-
-                treeBuilder.addFileEntry(currentEntry.name, entryContent)
+                val parentDirectory = getParentDirectory(currentEntry.name)
+                ImporterFeature.importFile(
+                    parent = parentDirectory,
+                    fileName = currentEntry.name.removePrefix(parentDirectory.name),
+                    fileStream = zipInputStream
+                )
             }
+
+            // if it is not a file entry, do not add the directory explicitly, as zip standard does not require
+            // directories to be even present. Just ignore empty directories, and add required ones on the fly.
 
             currentEntry = zipInputStream.nextEntry
         }
-
-//        return@transaction treeBuilder.create()
-        TODO()
     }
 
+    /**
+     * Return the [DirectoryEntity] that is the parent of the element denoted by [element]. If the directory does not
+     * exist yet, it and its parents are created recursively.
+     */
+    private fun getParentDirectory(element: String): DirectoryEntity {
+        val parentDirectoryPath = getParentPath(element)
+
+        return directoryMap.getOrPut(parentDirectoryPath) {
+            val directoryParent = getParentDirectory(parentDirectoryPath)
+            WorkspaceService.addDirectoryEntry(
+                name = parentDirectoryPath.removePrefix(directoryParent.name),
+                parent = directoryParent
+            )
+        }
+    }
+
+    /**
+     * Parse the path of a archive element's parent element
+     *
+     * @param path the element path
+     *
+     * @return the parent path used in the archive builder maps
+     */
+    private fun getParentPath(path: String): String {
+        return (if (path.endsWith("/"))
+            path.replace(lastDirectoryRegex, "")
+        else
+            path.removeRange(path.lastIndexOf('/').takeIf { it >= 0 }?.let { it + 1 }
+                ?: 0, path.length))
+            .ifEmpty { "/" }
+    }
 }
