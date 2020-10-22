1c640c4bcc37424403f63a7b9d50c331a801fa44
Cydhra <ubezl@student.kit.edu>
2020-10-22 21:15:30 +0200

    create schema

--- acromantula_backend/src/workspace/kotlin/net/cydhra/acromantula/workspace/DatabaseClient.kt
+++ acromantula_backend/src/workspace/kotlin/net/cydhra/acromantula/workspace/DatabaseClient.kt
@@ -1,7 +1,11 @@
 package net.cydhra.acromantula.workspace
 
+import net.cydhra.acromantula.workspace.disassembly.DisassemblyTable
+import net.cydhra.acromantula.workspace.filesystem.*
+import net.cydhra.acromantula.workspace.java.*
 import org.h2.jdbcx.JdbcDataSource
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.URL
@@ -28,7 +32,27 @@ internal class DatabaseClient(private val databasePath: String) {
                 it.setURL("jdbc:h2:$databasePath;DB_CLOSE_DELAY=10;MVCC=true")
             }
 
-        database = Database.connect(this.dataSource)
+        database = Database.connect(dataSource)
+
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(
+                DirectoryTable,
+                ArchiveTable,
+                FileTable,
+                IndexFileTable,
+                IndexMetaDatumTable,
+                DisassemblyTable,
+                JavaIdentifierTable,
+                JavaClassTable,
+                JavaMethodTable,
+                JavaFieldTable,
+                JavaAnnotationTable,
+                JavaModuleTable,
+                JavaParameterTable,
+                JavaSourceFileTable,
+                MemberReferenceTable,
+            )
+        }
     }
 
     /**
