318fdc2da3ef1f2e09f89b7b3c97a93ca55daed6
Flaflo <flaflo@ok.de>
2020-11-08 19:46:29 +0100

    :sparkles: Introducing IntelliJ codeflow

--- acromantula_gui/Acromantula GUI/CodeView/CodeViewMain.cs
+++ acromantula_gui/Acromantula GUI/CodeView/CodeViewMain.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using ScintillaNET;
 using ScintillaNET_FindReplaceDialog;
@@ -32,10 +34,18 @@ namespace Acromantula_GUI.CodeView
             editorContainer.Controls.Add(Scintilla);
 
             Scintilla.Dock = DockStyle.Fill;
-            Scintilla.Dock = DockStyle.Fill;
+
+            Scintilla.MultipleSelection = true;
+            Scintilla.MouseSelectionRectangularSwitch = true;
+            Scintilla.AdditionalSelectionTyping = true;
+            Scintilla.VirtualSpaceOptions = VirtualSpace.RectangularSelection;
 
             Scintilla.WrapMode = WrapMode.None;
             Scintilla.IndentationGuides = IndentView.LookBoth;
+
+            Scintilla.CharAdded += Scintilla_CharAdded;
+            Scintilla.InsertCheck += Scintilla_InsertCheck;
+            Scintilla.UpdateUI += Scintilla_UpdateUI;
 
             InitColors();
             InitSyntaxColoring();
@@ -114,7 +124,6 @@ namespace Acromantula_GUI.CodeView
         {
             Scintilla.KeyDown += OnHotkey;
 
-            // remove conflicting hotkeys from scintilla
             Scintilla.ClearCmdKey(Keys.Control | Keys.F);
             Scintilla.ClearCmdKey(Keys.Control | Keys.R);
             Scintilla.ClearCmdKey(Keys.Control | Keys.H);
@@ -124,7 +133,6 @@ namespace Acromantula_GUI.CodeView
 
         private void InitSyntaxColoring()
         {
-            // Configure the default style
             Scintilla.StyleResetDefault();
             Scintilla.Styles[Style.Default].Font = "Consolas";
             Scintilla.Styles[Style.Default].Size = 12;
@@ -137,6 +145,231 @@ namespace Acromantula_GUI.CodeView
             Scintilla.SetKeywords(0,
                 "abstract assert boolean break byte case catch char class const continue default do double else enum extends final finally float for goto if implements import instanceof int interface long native new package private protected public return short static strictfp super switch synchronized this throw throws transient try void volatile while");
             Scintilla.SetKeywords(1, "as fun in object is typeof typealias when val lateinit by var companion");
+        }
+
+        #endregion
+
+        #region CodeIndent
+
+        private void Scintilla_InsertCheck(object sender, InsertCheckEventArgs e)
+        {
+            if (e.Text.Length == 1)
+            {
+                var caretPos = Scintilla.CurrentPosition;
+                var charNext = Scintilla.GetCharAt(caretPos);
+
+                if (charNext == ')' && e.Text.ToLower() == ")" || charNext == '}' && e.Text.ToLower() == "}" ||
+                    charNext == ']' && e.Text.ToLower() == "]")
+                {
+                    var shouldSkip = charNext == ')' ||
+                                     charNext == '}' ||
+                                     charNext == ']';
+
+                    if (shouldSkip)
+                    {
+                        e.Text = "";
+                        Scintilla.GotoPosition(Scintilla.CurrentPosition + 1);
+                    }
+                }
+            }
+
+            if (!e.Text.EndsWith("\r") && !e.Text.EndsWith("\n")) return;
+
+            var line = Scintilla.Lines[Scintilla.LineFromPosition(Scintilla.CurrentPosition)];
+            var startPos = Scintilla.Lines[Scintilla.LineFromPosition(Scintilla.CurrentPosition)].Position;
+            var endPos = e.Position;
+                
+            var curLineText = Scintilla.GetTextRange(startPos, endPos - startPos);
+                    
+            var indent = Regex.Match(curLineText, "^[ \\t]*");
+                
+            e.Text += indent.Value;
+                
+            if (Regex.IsMatch(curLineText, "{\\s*$") && !line.Text.Trim().EndsWith("}"))
+            {
+                e.Text += "\t";
+            }
+        }
+
+        #endregion
+
+        #region Highlighting
+
+        private int _lastCaretPos;
+
+        private void Scintilla_UpdateUI(object sender, UpdateUIEventArgs e)
+        {
+            var caretPos = Scintilla.CurrentPosition;
+
+            if (_lastCaretPos == caretPos) return;
+
+            _lastCaretPos = caretPos;
+
+            var openingBracePos = -1;
+
+            if (caretPos > 0 && IsBrace(Scintilla.GetCharAt(caretPos - 1)))
+            {
+                openingBracePos = caretPos - 1;
+            }
+            else if (IsBrace(Scintilla.GetCharAt(caretPos)))
+            {
+                openingBracePos = caretPos;
+            }
+
+            if (openingBracePos != -1)
+            {
+                var closingBracePos = Scintilla.BraceMatch(openingBracePos);
+
+                if (closingBracePos == Scintilla.InvalidPosition)
+                {
+                    Scintilla.BraceBadLight(openingBracePos);
+                    Scintilla.HighlightGuide = 0;
+                }
+                else
+                {
+                    Scintilla.BraceHighlight(openingBracePos, closingBracePos);
+                    Scintilla.HighlightGuide = Scintilla.GetColumn(openingBracePos);
+                }
+            }
+            else
+            {
+                Scintilla.BraceHighlight(Scintilla.InvalidPosition, Scintilla.InvalidPosition);
+                Scintilla.HighlightGuide = 0;
+            }
+        }
+
+        #endregion
+
+        #region Autocompletion
+
+        private void Scintilla_CharAdded(object sender, CharAddedEventArgs e)
+        {
+            var currentLineIndex = Scintilla.LineFromPosition(Scintilla.CurrentPosition);
+            var currentLine = Scintilla.Lines[currentLineIndex];
+
+            switch (e.Char)
+            {
+                case '.':
+                {
+                    var list = new List<string> {"out", "in", "println", "currentTimeMillis"};
+                    list.Sort();
+                    Scintilla.AutoCShow(
+                        Scintilla.CurrentPosition - Scintilla.WordStartPosition(Scintilla.CurrentPosition, true),
+                        string.Join(" ", list));
+                    break;
+                }
+                case '}':
+                    if (Scintilla.Lines[currentLineIndex].Text.Trim() == "}")
+                    {
+                        currentLine.Indentation -= Scintilla.TabWidth;
+                    }
+
+                    break;
+                case '\n':
+                    var openingBraceLine = Scintilla.Lines[currentLineIndex - 2];
+                    var prevLine = Scintilla.Lines[currentLineIndex - 1];
+
+                    if (Regex.IsMatch(openingBraceLine.Text, "{\\s*$"))
+                    {
+                        Scintilla.InsertText(prevLine.EndPosition, "\n");
+                        currentLine.Indentation = prevLine.Indentation + Scintilla.TabWidth;
+                        Scintilla.GotoPosition(currentLine.Position + currentLine.Indentation);
+                    }
+
+                    break;
+                default:
+                    InsertMatchedChars(e);
+                    break;
+            }
+        }
+
+        private void InsertMatchedChars(CharAddedEventArgs e)
+        {
+            var caretPos = Scintilla.CurrentPosition;
+            var docStart = caretPos == 1;
+            var docEnd = caretPos == Scintilla.Text.Length;
+
+            var charPrev = docStart ? Scintilla.GetCharAt(caretPos) : Scintilla.GetCharAt(caretPos - 2);
+            var charNext = Scintilla.GetCharAt(caretPos);
+
+            var isCharPrevBlank = charPrev == ' ' || charPrev == '\t' ||
+                                  charPrev == '\n' || charPrev == '\r';
+
+            var isCharNextBlank = charNext == ' ' || charNext == '\t' ||
+                                  charNext == '\n' || charNext == '\r' ||
+                                  docEnd;
+
+            var isEnclosed = charPrev == '(' && charNext == ')' ||
+                             charPrev == '{' && charNext == '}' ||
+                             charPrev == '[' && charNext == ']';
+
+            var isSpaceEnclosed = charPrev == '(' && isCharNextBlank || isCharPrevBlank && charNext == ')' ||
+                                  charPrev == '{' && isCharNextBlank || isCharPrevBlank && charNext == '}' ||
+                                  charPrev == '[' && isCharNextBlank || isCharPrevBlank && charNext == ']';
+
+            var isCharOrString = isCharPrevBlank && isCharNextBlank || isEnclosed || isSpaceEnclosed;
+
+            var charNextIsCharOrString = charNext == '"' || charNext == '\'';
+
+            switch (e.Char)
+            {
+                case '(':
+                    if (charNextIsCharOrString) return;
+
+                    Scintilla.InsertText(caretPos, ")");
+
+                    break;
+                case '{':
+                    if (charNextIsCharOrString) return;
+
+                    if (isEnclosed)
+                    {
+                        Scintilla.GotoPosition(caretPos + 1);
+                    }
+                    else
+                    {
+                        Scintilla.InsertText(caretPos, "}");
+                    }
+
+                    break;
+                case '[':
+                    if (charNextIsCharOrString) return;
+
+                    if (isEnclosed)
+                    {
+                        Scintilla.GotoPosition(caretPos + 1);
+                    }
+                    else
+                    {
+                        Scintilla.InsertText(caretPos, "]");
+                    }
+
+                    break;
+                case '"':
+                    // 0x22 = "
+                    if (charPrev == 0x22 && charNext == 0x22)
+                    {
+                        Scintilla.DeleteRange(caretPos, 1);
+                        Scintilla.GotoPosition(caretPos);
+                        return;
+                    }
+
+                    if (isCharOrString)
+                        Scintilla.InsertText(caretPos, "\"");
+                    break;
+                case '\'':
+                    // 0x27 = '
+                    if (charPrev == 0x27 && charNext == 0x27)
+                    {
+                        Scintilla.DeleteRange(caretPos, 1);
+                        Scintilla.GotoPosition(caretPos);
+                        return;
+                    }
+
+                    if (isCharOrString)
+                        Scintilla.InsertText(caretPos, "'");
+                    break;
+            }
         }
 
         #endregion
@@ -247,11 +480,15 @@ namespace Acromantula_GUI.CodeView
 
         #region Utils
 
+        private static bool IsBrace(int c)
+        {
+            return c == '[' || c == ']' || c == '{' || c == '}' || c == '(' || c == ')';
+        }
+
         private static Color IntToColor(int rgb)
         {
             return Color.FromArgb(255, (byte) (rgb >> 16), (byte) (rgb >> 8), (byte) rgb);
         }
-
         #endregion
     }
 }
