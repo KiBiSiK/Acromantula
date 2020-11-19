77a34da40eccbdf7a4fd3d1b5fdbf0a030598854
Cydhra <ubezl@student.kit.edu>
2020-11-19 23:25:17 +0100

    added reconstructFromView method to ViewGeneratorStrategy

--- acromantula_backend/src/features/kotlin/net/cydhra/acromantula/features/view/ViewGeneratorStrategy.kt
+++ acromantula_backend/src/features/kotlin/net/cydhra/acromantula/features/view/ViewGeneratorStrategy.kt
@@ -23,6 +23,13 @@ interface ViewGeneratorStrategy {
     val fileType: FileType
 
     /**
+     * Whether this view generator supports regeneration of the original file from representation data. This can be
+     * used to edit the file using the representation as a surrogate, easing the editing process.
+     */
+    val supportsReconstruction: Boolean
+        get() = false
+
+    /**
      * Returns true, if this view generator is able to generate a view from the given file. False, if this strategy
      * cannot be used with this file
      */
@@ -32,4 +39,13 @@ interface ViewGeneratorStrategy {
      * Generate a view from the given file entity and return a handle to the generated view file.
      */
     fun generateView(fileEntity: FileEntity): FileRepresentation
+
+    /**
+     * Use the data from a view representation to reconstruct the file from it. The data must be of the [fileType]
+     * defined by this implementation. This method must not be called if [supportsReconstruction] is false, otherwise
+     * an [UnsupportedOperationException] is thrown.
+     */
+    fun reconstructFromView(fileEntity: FileEntity, buffer: ByteArray) {
+        throw UnsupportedOperationException("Reconstruction of file is unsupported for this view type")
+    }
 }
