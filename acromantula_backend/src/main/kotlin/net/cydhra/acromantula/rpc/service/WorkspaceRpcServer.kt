4e4bdc75e8fed4f2f7dcf5619cd13e1f01506239
Cydhra <ubezl@student.kit.edu>
2021-03-16 15:42:16 +0100

    rewritten listFiles rpc endpoint to stream its results

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/WorkspaceRpcServer.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/WorkspaceRpcServer.kt
@@ -1,10 +1,21 @@
 package net.cydhra.acromantula.rpc.service
 
 import com.google.protobuf.Empty
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.runBlocking
+import net.cydhra.acromantula.bus.EventBroker
 import net.cydhra.acromantula.commands.CommandDispatcherService
 import net.cydhra.acromantula.commands.interpreters.ListFilesCommandInterpreter
+import net.cydhra.acromantula.pool.Task
+import net.cydhra.acromantula.pool.event.TaskStatusChangedEvent
 import net.cydhra.acromantula.proto.*
 import net.cydhra.acromantula.workspace.WorkspaceService
+import net.cydhra.acromantula.workspace.filesystem.FileEntity
+import net.cydhra.acromantula.workspace.util.TreeNode
+import net.cydhra.acromantula.proto.FileEntity as ProtoFileEntity
 
 class WorkspaceRpcServer : WorkspaceServiceGrpcKt.WorkspaceServiceCoroutineImplBase() {
     override suspend fun listTasks(request: Empty): TaskListResponse {
@@ -21,16 +32,52 @@ class WorkspaceRpcServer : WorkspaceServiceGrpcKt.WorkspaceServiceCoroutineImplB
         }
     }
 
-    override suspend fun listFiles(request: ListFilesCommand): CommandResponse {
+    @ExperimentalCoroutinesApi
+    override fun listFiles(request: ListFilesCommand): Flow<ListFilesResponse> {
         val task = if (request.fileId != -1) {
             CommandDispatcherService.dispatchCommand(ListFilesCommandInterpreter(request.fileId))
         } else {
             CommandDispatcherService.dispatchCommand(ListFilesCommandInterpreter(request.filePath?.takeIf { it.isNotBlank() }))
         }
 
-        return commandResponse {
-            this.taskId = task.id
-            this.taskStatus = task.status
+        return callbackFlow {
+            val taskStatusChangedListener: suspend (TaskStatusChangedEvent) -> Unit = { event ->
+                if (event.taskId == task.id) {
+                    fun mapResultToProto(treeNode: TreeNode<FileEntity>): ProtoFileEntity {
+                        val children = treeNode.childList.map(::mapResultToProto)
+                        return fileEntity {
+                            id = treeNode.value.id.value
+                            name = treeNode.value.name
+                            isDirectory = treeNode.value.isDirectory
+                            children(*children.toTypedArray())
+                        }
+                    }
+
+                    @Suppress("UNCHECKED_CAST")
+                    val result = (WorkspaceService.getWorkerPool().reap(event.taskId) as
+                            Task<List<TreeNode<FileEntity>>>).result.get();
+
+                    result.onSuccess {
+                        offer(
+                            listFilesResponse {
+                                trees(*it.map(::mapResultToProto).toTypedArray())
+                            }
+                        )
+                        close()
+                    }
+                    result.onFailure { t ->
+                        close(cause = t)
+                    }
+                }
+            }
+
+            EventBroker.registerEventListener(TaskStatusChangedEvent::class, taskStatusChangedListener)
+
+            awaitClose {
+                runBlocking {
+                    EventBroker.unregisterEventListener(TaskStatusChangedEvent::class, taskStatusChangedListener)
+                }
+            }
         }
     }
 }
