9341859a29be99fad2a0970ac5a562da53ae2855
Cydhra <ubezl@student.kit.edu>
2021-03-16 17:12:38 +0100

    changed remaining proto interface to not use tasks anymore

--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/ExportRpcServer.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/ExportRpcServer.kt
@@ -1,27 +1,30 @@
 package net.cydhra.acromantula.rpc.service
 
 import com.google.protobuf.Empty
+import net.cydhra.acromantula.commands.CommandDispatcherService
+import net.cydhra.acromantula.commands.interpreters.ExportCommandInterpreter
 import net.cydhra.acromantula.features.exporter.ExporterFeature
 import net.cydhra.acromantula.proto.*
 
 class ExportRpcServer : ExportServiceGrpcKt.ExportServiceCoroutineImplBase() {
-    override suspend fun exportFile(request: ExportCommand): CommandResponse {
-//        val task = CommandDispatcherService.dispatchCommand(
-//            when {
-//                request.fileId != -1 -> ExportCommandInterpreter(request.fileId, request.exporter, request.targetPath)
-//                request.filePath != null -> ExportCommandInterpreter(
-//                    request.filePath,
-//                    request.exporter,
-//                    request.targetPath
-//                )
-//                else -> throw IllegalArgumentException("either fileId or filePath must be defined")
-//            }
-//        )
-//        return commandResponse {
-//            this.taskId = task.id
-//            this.taskStatus = task.status
-//        }
-        TODO()
+    override suspend fun exportFile(request: ExportCommand): Empty {
+        val result = CommandDispatcherService.dispatchCommand(
+            when {
+                request.fileId != -1 -> ExportCommandInterpreter(request.fileId, request.exporter, request.targetPath)
+                request.filePath != null -> ExportCommandInterpreter(
+                    request.filePath,
+                    request.exporter,
+                    request.targetPath
+                )
+                else -> throw IllegalArgumentException("either fileId or filePath must be defined")
+            }
+        ).await()
+
+        result.onFailure {
+            throw it
+        }
+
+        return Empty.getDefaultInstance()
     }
 
     override suspend fun getExporters(request: Empty): ExportersList {
--- acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/ImportRpcServer.kt
+++ acromantula_backend/src/main/kotlin/net/cydhra/acromantula/rpc/service/ImportRpcServer.kt
@@ -1,24 +1,27 @@
 package net.cydhra.acromantula.rpc.service
 
-import net.cydhra.acromantula.proto.CommandResponse
+import com.google.protobuf.Empty
+import net.cydhra.acromantula.commands.CommandDispatcherService
+import net.cydhra.acromantula.commands.interpreters.ImportCommandInterpreter
 import net.cydhra.acromantula.proto.ImportCommand
 import net.cydhra.acromantula.proto.ImportServiceGrpcKt
 
 class ImportRpcServer : ImportServiceGrpcKt.ImportServiceCoroutineImplBase() {
 
-    override suspend fun importFile(request: ImportCommand): CommandResponse {
-//        val task = CommandDispatcherService.dispatchCommand(
-//            when {
-//                request.directoryId != -1 ->
-//                    ImportCommandInterpreter(request.directoryId, request.fileUrl)
-//                request.directoryPath != null -> ImportCommandInterpreter(request.directoryPath, request.fileUrl)
-//                else -> throw IllegalArgumentException("either directoryId or directoryPath must be defined")
-//            }
-//        )
-//        return commandResponse {
-//            this.taskId = task.id
-//            this.taskStatus = task.status
-//        }
-        TODO()
+    override suspend fun importFile(request: ImportCommand): Empty {
+        val result = CommandDispatcherService.dispatchCommand(
+            when {
+                request.directoryId != -1 ->
+                    ImportCommandInterpreter(request.directoryId, request.fileUrl)
+                request.directoryPath != null -> ImportCommandInterpreter(request.directoryPath, request.fileUrl)
+                else -> throw IllegalArgumentException("either directoryId or directoryPath must be defined")
+            }
+        ).await()
+
+        result.onFailure {
+            throw it
+        }
+
+        return Empty.getDefaultInstance()
     }
 }
