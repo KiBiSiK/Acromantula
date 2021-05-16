edadf3ae98a9c80bc790a0c5cb69caddd0c057b8
Cydhra <ubezl@student.kit.edu>
2021-05-16 14:36:16 +0200

    removed task remnants from rpc interface and event system

--- Bus/src/main/kotlin/net/cydhra/acromantula/pool/event/TaskFinishedEvent.kt
+++
@@ -1,5 +0,0 @@
-package net.cydhra.acromantula.pool.event
-
-import net.cydhra.acromantula.bus.Event
-
-data class TaskFinishedEvent(val taskId: Int) : Event
--- src/main/kotlin/net/cydhra/acromantula/rpc/service/BusRpcServer.kt
+++ src/main/kotlin/net/cydhra/acromantula/rpc/service/BusRpcServer.kt
@@ -1,4 +1,4 @@
-package net.cydhra.acromantula.rpc.service
+ package net.cydhra.acromantula.rpc.service
 
 import com.google.protobuf.Empty
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -10,8 +10,6 @@ import kotlinx.coroutines.runBlocking
 import net.cydhra.acromantula.bus.EventBroker
 import net.cydhra.acromantula.bus.events.ApplicationShutdownEvent
 import net.cydhra.acromantula.bus.events.ApplicationStartupEvent
-import net.cydhra.acromantula.pool.event.TaskFinishedEvent
-import net.cydhra.acromantula.pool.event.TaskStatusChangedEvent
 import net.cydhra.acromantula.proto.*
 
 class BusRpcServer : BusServiceGrpcKt.BusServiceCoroutineImplBase() {
@@ -37,35 +35,14 @@ class BusRpcServer : BusServiceGrpcKt.BusServiceCoroutineImplBase() {
                 this.cancel("graceful shutdown")
             }
 
-            val taskFinishedListener: suspend (TaskFinishedEvent) -> Unit = { event ->
-                offer(bus_Event {
-                    this.taskFinishedEvent = bus_TaskFinishedEvent {
-                        taskId = event.taskId
-                    }
-                })
-            }
-
-            val taskStatusChangedListener: suspend (TaskStatusChangedEvent) -> Unit = { event ->
-                bus_Event {
-                    this.taskStatusChangedEvent = bus_TaskStatusChangedEvent {
-                        taskId = event.taskId
-                        newStatus = event.newStatus
-                    }
-                }
-            }
-
             EventBroker.registerEventListener(ApplicationStartupEvent::class, applicationStartupListener)
             EventBroker.registerEventListener(ApplicationShutdownEvent::class, applicationShutdownListener)
-            EventBroker.registerEventListener(TaskFinishedEvent::class, taskFinishedListener)
-            EventBroker.registerEventListener(TaskStatusChangedEvent::class, taskStatusChangedListener)
 
             awaitClose {
                 // unregister the listeners of this flow, if the flow is closed (most likely by the client)
                 runBlocking {
                     EventBroker.unregisterEventListener(ApplicationStartupEvent::class, applicationStartupListener)
                     EventBroker.unregisterEventListener(ApplicationShutdownEvent::class, applicationShutdownListener)
-                    EventBroker.unregisterEventListener(TaskFinishedEvent::class, taskFinishedListener)
-                    EventBroker.unregisterEventListener(TaskStatusChangedEvent::class, taskStatusChangedListener)
                 }
             }
         }
