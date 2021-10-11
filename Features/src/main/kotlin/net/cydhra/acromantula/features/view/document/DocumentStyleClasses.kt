720c4a950a3d016d64e2c3f1ef26d392321ba609
Cydhra <ubezl@student.kit.edu>
2021-10-12 01:30:12 +0200

    new style classes

--- Features/src/main/kotlin/net/cydhra/acromantula/features/view/document/DocumentStyleClasses.kt
+++ Features/src/main/kotlin/net/cydhra/acromantula/features/view/document/DocumentStyleClasses.kt
@@ -4,6 +4,7 @@ package net.cydhra.acromantula.features.view.document
 value class StyleClass(val cls: String)
 
 val STYLE_KEYWORD: StyleClass = StyleClass("keyword")
+val STYLE_OPERATION: StyleClass = StyleClass("operation")
 
 val STYLE_IDENTIFIER: StyleClass = StyleClass("identifier")
 val STYLE_TYPE: StyleClass = StyleClass("type")
@@ -13,6 +14,7 @@ val STYLE_LITERAL_STRING = STYLE_LITERAL + StyleClass("string")
 val STYLE_LITERAL_NUMBER = STYLE_LITERAL + StyleClass("numeric")
 
 val STYLE_ANNOTATION: StyleClass = StyleClass("annotation")
+val STYLE_LABEL: StyleClass = StyleClass("label")
 
 val STYLE_PUNCTUATION: StyleClass = StyleClass("punctuation")
 val STYLE_PARENTHESIS: StyleClass = StyleClass("parenthesis")
