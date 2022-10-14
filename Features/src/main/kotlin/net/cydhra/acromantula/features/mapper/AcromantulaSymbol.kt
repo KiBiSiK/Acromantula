56e4fd32e57462a79aaf0c6ff2813b106b3baff9
Cydhra <ubezl@student.kit.edu>
2022-10-14 16:53:06 +0200

    split symbol.name into getter and update-setter because the setter is supposed to be suspending

--- Features/src/main/kotlin/net/cydhra/acromantula/features/mapper/AcromantulaSymbol.kt
+++ Features/src/main/kotlin/net/cydhra/acromantula/features/mapper/AcromantulaSymbol.kt
@@ -10,17 +10,21 @@ interface AcromantulaSymbol {
     val canBeRenamed: Boolean
 
     /**
-     * The actual symbol content that is referenced by the data structures of the mapped file. This does not need to
-     * be unique. Using the setter of this property will rename the symbol and update all references iff
-     * [canBeRenamed] is true. The implementation of this interface is responsible for calling
-     * [AcromantulaReference.onUpdateSymbolName] on all references.
-     */
-    var name: String
-
-    /**
      * Where this symbol is located, or null if this is an inferred external symbol
      */
     val sourceFile: FileEntity?
+
+    /**
+     * The actual symbol content that is referenced by the data structures of the mapped file. This does not need to
+     * be unique. Use [updateName] to update the name and all references if [canBeRenamed] is true.
+     */
+    fun getName(): String
+
+    /**
+     * Rename the symbol and update all references iff [canBeRenamed] is true. The implementation of this interface
+     * is responsible for calling [AcromantulaReference.onUpdateSymbolName] on all references.
+     */
+    suspend fun updateName(newName: String)
 
     /**
      * Generate a string representation of this symbol for user interfaces
