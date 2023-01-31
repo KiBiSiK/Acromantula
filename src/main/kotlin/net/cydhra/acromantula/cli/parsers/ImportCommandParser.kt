4108bbb2adc72c6b0e2eecfc0f0f68fb26c5e52f
Cydhra <ubezl@student.kit.edu>
2023-01-31 02:43:01 +0100

    report partial results of importer using a channel

--- src/main/kotlin/net/cydhra/acromantula/cli/parsers/ImportCommandParser.kt
+++ src/main/kotlin/net/cydhra/acromantula/cli/parsers/ImportCommandParser.kt
@@ -2,9 +2,17 @@ package net.cydhra.acromantula.cli.parsers
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import net.cydhra.acromantula.cli.WorkspaceCommandParser
 import net.cydhra.acromantula.commands.WorkspaceCommandInterpreter
 import net.cydhra.acromantula.commands.interpreters.ImportCommandInterpreter
+import net.cydhra.acromantula.features.importer.ImporterJob
+import net.cydhra.acromantula.workspace.filesystem.FileEntity
 import org.apache.logging.log4j.LogManager
 
 class ImportCommandParser(parser: ArgParser) : WorkspaceCommandParser<Unit> {
@@ -12,16 +20,30 @@ class ImportCommandParser(parser: ArgParser) : WorkspaceCommandParser<Unit> {
         private val logger = LogManager.getLogger()
     }
 
-    val directory by parser
-        .storing(
-            "-d", "--directory",
-            help = "path where to place the file in the workspace file tree. leave empty for workspace root.",
-        )
-        .default(null)
+    private val directory by parser.storing(
+        "-d", "--directory",
+        help = "path where to place the file in the workspace file tree. leave empty for workspace root.",
+    ).default(null)
 
-    val fileUrl by parser.positional("URL", help = "URL pointing to the file")
+    private val fileUrl by parser.positional("URL", help = "URL pointing to the file")
 
-    override fun build(): WorkspaceCommandInterpreter<Unit> = ImportCommandInterpreter(directory, fileUrl)
+    override fun build(): WorkspaceCommandInterpreter<Unit> {
+        val partialResultChannel = Channel<ImporterJob.ImportProgressEvent>(2)
+        partialResultChannel.consumeAsFlow().onEach { event ->
+            when (event) {
+                is ImporterJob.ImportProgressEvent.ImportComplete -> {
+                    fun count(file: FileEntity): Int = 1 + file.children.map(::count).sum()
+
+                    logger.info("successfully imported ${count(event.root)} files. Waiting for mapping...")
+                }
+
+                else -> {/* do nothing */
+                }
+            }
+        }.launchIn(CoroutineScope(Dispatchers.Default))
+
+        return ImportCommandInterpreter(directory, fileUrl, partialResultChannel)
+    }
 
     override fun report(result: Result<Unit>) {
         logger.info("import of \"$fileUrl\" ${if (result.isSuccess) "successful" else "failed"}")
